a1080702f51cd70d9c5d0d8852f1b908a774fd83
Jimmy Larsson <jl90@live.se>
2012-03-20 12:30:36 +0100

    Request added //Following Martin's instructions (SO DON'T BLAME ME!)

---
+++ src/request/Request.py
@@ -0,0 +1,18 @@
+'''
+Created on Mar 20, 2012
+
+@author: Jimmy
+@version: 0.1
+'''
+
+from addalyse import *
+
+'''
+    Requests a certain Twitter username to be added. 
+    @argument username: A string containing the username of a Twitter user.
+    @return: A boolean set to true if the user has been added, otherwise false.
+'''
+
+def request_add(username):
+    addalyse(username,0,False)
+    
