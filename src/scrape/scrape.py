10f2f03c40c4b50507fe9f6eff812d5238df3e8f
mbernt <mbernt@karmosin-13.csc.kth.se>
2012-03-20 16:28:06 +0100

    added scrape. currently does nothing.

---
+++ src/scrape/scrape.py
@@ -0,0 +1,20 @@
+'''
+Created on Mar 20, 2012
+
+@author: mbernt
+'''
+
+    
+def main():
+    '''Adds users from twitter to database.'''
+    while(True):
+        None
+        # get a twitter user from twitter
+        
+        # add addalyse user if not in database
+        
+        # sleep
+    
+
+if __name__ == "__main__":
+    main()
