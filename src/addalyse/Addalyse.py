e63a2759ddd1ecbb303c5885ee75d33eb3b97623
mbernt <mbernt@karmosin-13.csc.kth.se>
2012-03-20 14:31:32 +0100

    small update of addalyse

--- src/addalyse/Addalyse.py
+++ src/addalyse/Addalyse.py
@@ -33,8 +33,9 @@ def addalyse(username,since_ID,remake_profile):
     
     if remakeProfile is false it will analyse tweets newer than sinceID and merge the result with the profile in solr
     '''
-    # maybe check if the user exists on twitter
-    
+    # maybe check if the user exists on twitter, but this check might be done in get_all_tweets
+    #if(!TwitterHelp.contains(username)):
+    #    return False
 
     
     if(remake_profile):
@@ -42,13 +43,13 @@ def addalyse(username,since_ID,remake_profile):
         #tweets = TwitterHelp.get_all_tweets(username)
         #if(tweets==None || tweets.length()==0):
         #    return False
-        # profile = TwitterHelp.get_profile() #see in solr schema what is needed
+        #profile = TwitterHelp.get_profile() #see in solr schema what is needed
         
         # send to analysis
-        # analysis=analysis.analyse(tweets)
+        #analysis=analysis.analyse(tweets)
         
         # store result in sunburnt
-        # Storage_handler.add_profile(username,profile,analysis)
+        #Storage_handler.add_profile(username,profile,analysis)
         
         return True #returns true if added to solr
     else:
@@ -58,10 +59,10 @@ def addalyse(username,since_ID,remake_profile):
         #    return False
         
         # send to analysis
-        # analysis=analysis.analyse(tweets)
+        #analysis=analysis.analyse(tweets)
         
         # merge result with the profile in solr
-        # Storage_handler.update_profile(username,analysis)
+        #Storage_handler.update_profile(username,analysis)
         
         return True # returns true if merged with solr
         
