55d4c7b8ccd1ae4cccea638787966e6aca34bfe0
Anton Kindestam <bountyjedi@gmail.com>
2012-03-20 18:05:00 +0100

    Mer cleanup i update.py

--- src/update/update.py
+++ src/update/update.py
@@ -1,8 +1,10 @@
 '''
 Created on Mar 20, 2012
 
-@author: mbernt
-@version: 0.2
+The program that updates users that are already in the database so
+that they may be up to date.
+
+TODO: WRITE MORE DESCRIPTION OF ME?
 '''
 
 from addalyse import *
@@ -13,13 +15,13 @@ import time
 def main():
     '''Gets profiles from storageHandler and checks if they need
     updating, and if so updates those.'''
-    twitter_help=TwitterHelp()
-    limit=100              # do complete update every hundredth update
+    twitter_help = TwitterHelp() 
+    limit = 100            # do complete update every hundredth update
     while(True):
-        list=StorageHandler.get_all_user_for_update()
+        list = StorageHandler.get_all_user_for_update()
         for (since_id, update_count, username) in list:
             if since_id != twitter_help.get_latest_since_id(username): # check if need updating
-                addalyse(username,since_id,update_count>limit) # replace if update_count>limit else update
+                addalyse(username, since_id, update_count > limit) # replace if update_count>limit else update
             time.sleep(10) # sleep for ten seconds, to not make to many requests to twitter
     
 if __name__ == "__main__":
