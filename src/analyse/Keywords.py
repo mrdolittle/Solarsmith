efa622872d540f5ee9aea27be2b653e2e060cd94
RaxXo <obodemyr@kth.se>
2012-03-20 12:42:14 +0100

    Git är fett buggigt

--- src/analyse/Keywords.py
+++ src/analyse/Keywords.py
@@ -13,10 +13,8 @@ def extract_keywords(string):
     '''Receives a string and returns a list of extracts keywords'''
     
     sequence=nltk.pos_tag(nltk.word_tokenize(string))
-    words=[]
-    x=0;
     
-    while(x<(len(sequence)-1)):
+    def doStuff(x, words):
         '''Recursive help method to parse for keywords and keyword sequences'''
         (thisWord, this) = sequence[x]
         (nextWord, nxt) = sequence[x+1]
@@ -33,10 +31,13 @@ def extract_keywords(string):
             
         #Checks for simple nouns
         if this in ['NN','NNS','NNP','VBG']:
-            words.append(thisWord)
-        x=x+1
+            words.append(thisWord)        
+        if(x+1<(len(sequence)-1)):
+            return doStuff(x+1, words)
+        else:
+            return filter_keywords(words)
             
-    return filter_keywords(words)      
+    return doStuff(0,[])      
     
     
                     
