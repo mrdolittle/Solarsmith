bda4367909a60ff0907c93e102f5748df036ad53
mbernt <mbernt@karmosin-13.csc.kth.se>
2012-03-20 17:22:19 +0100

    update may be functional now.

--- src/update/update.py
+++ src/update/update.py
@@ -12,14 +12,15 @@ from twitterHelp import *
 def main():
     '''Gets profiles from storageHandler and checks if they need updating, and if so 
     updates those.'''
-    limit=100
+    twitter_help=TwitterHelp()
+    limit=100 # do complete update every hundredth update
     while(True):
         list=StorageHandler.get_all_user_for_update()
         for tuple in list:
             (since_id,update_count,username)=tuple
-            #if (since_id!=twitterHelp.get_latest_since_id(username):#check if need updating
-            addalyse(username,since_id,update_count>limit)#replace if update_count>limit
-        #sleep(1000)
+            if since_id!=twitter_help.get_latest_since_id(username):#check if need updating
+                addalyse(username,since_id,update_count>limit)#replace if update_count>limit else update
+            #sleep(1000) #sleep to not make to many requests to twitter
     
 if __name__ == "__main__":
     main()
