aac03bf9442e10f41463190aa76de8d01fb87dd5
Anton Kindestam <bountyjedi@gmail.com>
2012-03-20 17:55:32 +0100

    Cleanup.

--- src/update/update.py
+++ src/update/update.py
@@ -11,17 +11,16 @@ from twitterHelp import *
 import time
 
 def main():
-    '''Gets profiles from storageHandler and checks if they need updating, and if so 
-    updates those.'''
+    '''Gets profiles from storageHandler and checks if they need
+    updating, and if so updates those.'''
     twitter_help=TwitterHelp()
-    limit=100 # do complete update every hundredth update
+    limit=100              # do complete update every hundredth update
     while(True):
         list=StorageHandler.get_all_user_for_update()
-        for tuple in list:
-            (since_id,update_count,username)=tuple
-            if since_id!=twitter_help.get_latest_since_id(username):#check if need updating
-                addalyse(username,since_id,update_count>limit)#replace if update_count>limit else update
-            time.sleep(10)#sleep for ten seconds, to not make to many requests to twitter
+        for (since_id, update_count, username) in list:
+            if since_id != twitter_help.get_latest_since_id(username): # check if need updating
+                addalyse(username,since_id,update_count>limit) # replace if update_count>limit else update
+            time.sleep(10) # sleep for ten seconds, to not make to many requests to twitter
     
 if __name__ == "__main__":
     main()
