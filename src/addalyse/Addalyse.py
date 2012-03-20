79738298dc2666bddd9f986e5f9df13567f5c038
mbernt <mbernt@karmosin-13.csc.kth.se>
2012-03-20 14:54:34 +0100

    updated comments.

--- src/addalyse/Addalyse.py
+++ src/addalyse/Addalyse.py
@@ -16,25 +16,29 @@ def set_globvar():
     # do something
     a=twitter_help
 
-def addalyse(username,since_ID,remake_profile):
+def addalyse(username,since_id,remake_profile):
     '''
-    Used by: update, scrape and request
-    
-    need to access via a connection:
-    twitter_API, sunburnt_API
-    
-    API: (boolean userUpdatedInDatabase) addalyse(String username,int sinceID,boolean remakeProfile):
-    
-    communicates with: Sunburnt and twitter API
-    
     Description:
+    Directly returns false if the twitter user isn't on twitter.
+    
     if remakeProfile is true then it will disregard sinceID and analyse as many tweets as possible
     and then replace the profile in solr.
     
     if remakeProfile is false it will analyse tweets newer than sinceID and merge the result with the profile in solr
+    
+    Returns: True if successful else False
+    Exceptions:
+    Input types:  addalyse(String username,int sinceID,boolean remakeProfile):
+    Signature:  (boolean succesfull_add) addalyse(String username,int since_id,boolean remake_profile):
+    
+    Used by: 
+    update, scrape and request
+    
+    need to access via a connection:
+    twitter_API, sunburnt_API
     '''
     # maybe check if the user exists on twitter, but this check might be done in get_all_tweets
-    #if(!TwitterHelp.contains(username)):
+    #if !TwitterHelp.contains(username):
     #    return False
 
     
