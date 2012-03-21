a59920102bcf0703031c9081b368d5804b50ab8c
ch <ch@SC2>
2012-03-21 11:46:52 +0100

    ville bara bli av med pilen

--- src/analyse/MovieReviewBayes.py
+++ src/analyse/MovieReviewBayes.py
@@ -10,47 +10,34 @@ from nltk.corpus import movie_reviews
 def word_feats(words):
     return dict([(word, True) for word in words])
  
-negids = movie_reviews.fileids('neg')
-posids = movie_reviews.fileids('pos')
-
- 
-negfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in negids]
-posfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in posids]
-
-#for f in negids:
-#    print movie_reviews.words([f])
+def main():
     
-negcutoff = len(negfeats)*3/4
-poscutoff = len(posfeats)*3/4
+    negids = movie_reviews.fileids('neg')
+    posids = movie_reviews.fileids('pos')
+    
+     
+    negfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in negids]
+    posfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in posids]
+    
+        
+    negcutoff = len(negfeats)*3/4
+    poscutoff = len(posfeats)*3/4
+     
+    trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
+    testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
+    print 'train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats))
+    print  testfeats[450]
+    classifier = NaiveBayesClassifier.train(trainfeats)
+    print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
+    classifier.show_most_informative_features()
+    
  
-trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
-testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
-print 'train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats))
- 
-classifier = NaiveBayesClassifier.train(trainfeats)
-print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
-classifier.show_most_informative_features()
 
-#x = raw_input('Write a sentence to be analysed!')
-x=['I','fascination','car']
-print classifier.classify(word_feats(x));
-print word_feats(x)
-x=['I','uninvolving','car']
-muahahahhahahahha= 'hiihihihihiihihihihiihih' ,
-                    'hahahahhahahahahhahahah'
-print classifier.classify(word_feats(x));
-print word_feats(x)
-x=['I','idiotic','cugsdfar']
-print classifier.classify(word_feats(x));
-print word_feats(x)
+    while True:
+        x = raw_input("type string to analyse! \n")
+        token=nltk.word_tokenize(x)
+        print word_feats(token)
+        print classifier.classify(word_feats(token))
 
-
-
-
-
-
-
-
-
-
-
+if __name__ =="__main__":
+    main()
