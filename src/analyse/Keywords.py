10248130aec36db191094a458d5d8259cdab23b6
RaxXo <obodemyr@kth.se>
2012-03-22 12:55:47 +0100

    Added method extract which extracts hashtags and keywords

--- src/analyse/Keywords.py
+++ src/analyse/Keywords.py
@@ -47,11 +47,30 @@ def extract_keywords(string):
 
     return doStuff(0, [])
 
-#In development
-def extract_keywords_chunks(text):
-    '''Uses chunks matching to identify keywords in a tweet. The code looks much nicer this way :P'''
+def strip_hashtags(text,words):
+    '''Searches for hashtags and adds the tag to the list of keywords.
+        Removes these from the sequence so that it can later be used by extract_keywords_grammar'''
     sequence=reduce(operator.add, map(nltk.pos_tag, map(nltk.word_tokenize, nltk.sent_tokenize(text))))
-    print sequence
+    length=len(sequence)
+    x=0;
+    while (x<length):
+        elem=sequence[x]
+        if elem[0]=='#':
+            del sequence[x]
+            length=length-1     
+            if x<length:
+                words.append(sequence[x][0])
+                del sequence[x]
+                length=length-1
+                x=x-1
+            x=x-1
+        x=x+1
+    print words
+    return (words,sequence)          
+        
+#Using grammar
+def extract_keywords_grammar(words,sequence):
+    '''Uses chunks matching to identify keywords in a tweet. The code looks much nicer this way :P'''           
     grammar=''' Noun: {<DT>?<JJ>+(<NN>|<NNS>|<VBG>)+}
                 ToVerb: {<TO><VB>}
                 Name:{<NNP>+}                
@@ -59,7 +78,6 @@ def extract_keywords_chunks(text):
     grammarSingular='''Noun:{(<NN>|<NNS>|<VBG>)}
                         Name: {<NNP>}
     '''
-    words = []
     chunks = nltk.RegexpParser(grammar)
     chunksSingular = nltk.RegexpParser(grammarSingular)
     
@@ -78,11 +96,15 @@ def extract_keywords_chunks(text):
             words.append(s[0][0])
                     
     return words
+
+def extract(text,words):
+    hashtagreturn = strip_hashtags(text,words)
+    return extract_keywords_grammar(hashtagreturn[0],hashtagreturn[1])
                     
 #Initialize _POS_TAGGER
 nltk.data.load(_POS_TAGGER)
 
 if __name__ == '__main__':
-    text = "Bear Grylls likes to go spear fishing :) #fishing"
+    text = "Bear Grylls likes to go spear fishing #fishing"
     print extract_keywords(text)
-    print extract_keywords_chunks(text)
+    print extract(text,[])
