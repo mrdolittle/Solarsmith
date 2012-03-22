018505c4b66e9fb037cf49f4df488748f723de9f
Anton Kindestam <bountyjedi@gmail.com>
2012-03-22 15:31:13 +0100

    Change Document.py to use the safe ast.literal_eval()

--- src/storageHandler/Document.py
+++ src/storageHandler/Document.py
@@ -2,6 +2,8 @@
 Module for the eminent class Document
 '''
 
+import ast
+
 
 def weight_total(lst):
     '''Returns the total weight of a list of (keyword, weight) tuples.'''
@@ -29,6 +31,8 @@ def keyword_list_to_text(lst):
             
     return "\n".join(stringsies)
 
+
+
 class Document:
     '''Class for handling our Solr documents neatly together with Sunburnt.
 
@@ -40,20 +44,7 @@ class Document:
            since the fields used in our schema are named:
            lovekeywords_list/hatekeywords_list we can actually just
            add an object of this class to Solr's database elegantly
-           using Sunburnt, while excluding the _pylist ones.
-
-    TODO: Maybe store the stuff using some other format (JSON) rather
-          than using repr which forces us to use eval to get the data
-          back from the string format it is inevitably stored as later
-          (due to the schema we are using).
-
-          Though the crazy usage of eval is not as bad as it looks
-          since we are using repr when storing we can be pretty safe
-          that any sort of injectiony thing will not be inserted. At
-          least not by this class. However if someone had raw access
-          to Solr.... (but in a proper setting it should be
-          protected!!!)'''
-
+           using Sunburnt, while excluding the _pylist ones.'''
     
     def __init__(self,
                  id,
@@ -63,10 +54,9 @@ class Document:
                  updatecount = 0):
         self.id = id
 
-        # WOOP WOOP: I did warn ya that eval was up-a-coming!
         # Parse the args if they are strings (otherwise they would normally be lists of tuples etc.etc.)
-        self.lovekeywords_pylist = eval(lovekeywords_list) if isinstance(lovekeywords_list, str) else lovekeywords_list
-        self.hatekeywords_pylist = eval(hatekeywords_list) if isinstance(hatekeywords_list, str) else hatekeywords_list
+        self.lovekeywords_pylist = ast.literal_eval(lovekeywords_list) if isinstance(lovekeywords_list, str) else lovekeywords_list
+        self.hatekeywords_pylist = ast.literal_eval(hatekeywords_list) if isinstance(hatekeywords_list, str) else hatekeywords_list
         
         self.since_id = since_id
         self.updatecount = updatecount
