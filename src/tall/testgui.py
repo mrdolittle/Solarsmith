071b19cc221eba841fd60992ea0c0926f39b1c6f
Petter Djupfeldt <petterdjupfeldt@gmail.com>
2012-03-26 11:45:15 +0200

    Added documentation to tall and started improving tallstore

--- src/tall/testgui.py
+++ src/tall/testgui.py
@@ -4,12 +4,16 @@ Created on Mar 21, 2012
 @author: Jonas & Petter
 '''
 import httplib
+import tallstore
 
+#def client(string):
+#    host = httplib.HTTPConnection('localhost', 8001, timeout=10)
+#    host.request("GET" + string, 'localhost:8001')
+#    print host.getresponse(True).read(None)
+#
+#print "nagonting"
+#client('hej pa servern')
 
-def client(string):
-    host = httplib.HTTPConnection('localhost', 8001, timeout=10)
-    host.request("GET" + string, 'localhost:8001')
-    print host.getresponse(True).read(None)
-
-print "nagonting"
-client('hej pa servern')
+list = tallstore.get_list_from_string("[('cat', 34), ('fishing', 22), ('bear grylls', 33)]")
+for temp, asd in list:
+    print temp
