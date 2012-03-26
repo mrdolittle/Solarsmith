a497fd0ac84e68ef598c4af91243b6274afd44bf
Anton Kindestam <bountyjedi@gmail.com>
2012-03-26 13:15:09 +0200

    We're still in mockup-phase as far as __init__.py is concerned. Don't import unneccesary stuff that we aren't to expose anyhow!

--- src/analyse/__init__.py
+++ src/analyse/__init__.py
@@ -1,5 +1,5 @@
-from .Stopwords import filter_keywords
-from .Keywords import extract_keywords
+# from .Stopwords import filter_keywords
+# from .Keywords import extract_keywords
 #__all__ = ['extract_keywords', 'filter_keywords']
 
 
