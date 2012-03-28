128c8f2427673217d38a8fae8723fdd285150fb4
RaxXo <obodemyr@kth.se>
2012-03-28 12:52:04 +0200

    Loop for searching for words and tagging results

--- src/analyse/manualtag.py
+++ src/analyse/manualtag.py
@@ -1,27 +1,30 @@
-writefile = open('manual', 'w')
 import twitter
-api = twitter.Api()
-var = raw_input("What would you like to search for?")
-searchresults = a.GetSearch(var)
- 
-for line in searchresults:
-    line = line.text()
-    print line + "\n"
-    var = raw_input("Enter an option: [Positive: '+', Negative: '-', Neutral: '0', Skip: 's']")
-    if var == '+':
-        par = (line, "positive")
-        writefile.write(repr(par))
-        writefile.write("\n")
-    if var == '-':
-        par = (line, "negative")
-        writefile.write(repr(par))
-        writefile.write("\n")
-    if var == '0':
-        par = (line, "neutral")
-        writefile.write(repr(par))
-        writefile.write("\n")
 
-readfile.close()
+writefile = open('manual', 'a')
+api = twitter.Api()
+option = raw_input("What would you like to search for? \n")
+while option !="":
+    searchresults = api.GetSearch(option)
+ 
+    for line in searchresults:
+        line = line.text
+        print line
+        var = raw_input("Enter an option: [Positive: '+', Negative: '-', Neutral: '0', Skip: 's'] \n")
+        if var == '+':
+            a = (line, "positive")
+            writefile.write(repr(a))
+            writefile.write("\n")
+        if var == '-':
+            a = (line, "negative")
+            writefile.write(repr(a))
+            writefile.write("\n")
+        if var == '0':
+            a = (line, "neutral")
+            writefile.write(repr(a))
+            writefile.write("\n")
+            
+    print "DONE!"
+    
 writefile.close()
 
         
