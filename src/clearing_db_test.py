cccaa9883b272bae2b0acc2ab80d8b86b259456a
Anton Kindestam <bountyjedi@gmail.com>
2012-03-29 12:23:10 +0200

    clearing_db_test no longer deletes everything from solr and prefers to just overwrite all the test users

--- src/clearing_db_test.py
+++ src/clearing_db_test.py
@@ -13,12 +13,12 @@ SOLR_SERVER = "http://xantoz.failar.nu:8080/solr/"
 def clear_database_and_add_users(usernames=["SSDummy_Janet", "ssdummy_henry", "ssdummy_hoot", "ssdummy_faye"
                                             ,"ssdummy_burt", "ssdummy_gustavo", "ssdummy_amanda"
                                             ,"ssdummy_duke", "ssdummy_ian", "ssdummy_ellen" , "ssdummy_chrissy"]):
-    
-    '''Used by analyse team to test their accuracy!
-    Removes all users from the database and then adds the ones in the usernames list.
+    '''Used by analyse team to test their accuracy!  Removes all users
+    from the database and then adds the ones in the usernames list.
     TODO: test it'''
+    
     sh = StorageHandler(SOLR_SERVER)
-    sh.delete_all()
+    # sh.delete_all()
     users_left_to_add = len(usernames)
     print "CLEARING DATABASE!"
     for username in usernames:
