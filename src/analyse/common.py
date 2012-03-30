3d980b4e9e608e81d79272baab856999b6309c59
Anton Kindestam <bountyjedi@gmail.com>
2012-03-30 20:27:32 +0200

    Onödigt att returna lista med tom sträng. Returnerar tom lista som ändå konkateneras och dessutom snyggare!

--- src/analyse/common.py
+++ src/analyse/common.py
@@ -25,9 +25,9 @@ def split_tweet(text):
             [(a, b)] = re.findall(r'([#@]\w+)(.*)', tag) # this will blow up if more than one match (but it won't due to the regex...)
             return [a] if b == '' else [a,b]
         except ValueError:
-            # KLUDGE: When we had a too short string or otherwise failed return a string with a space (to not confuse the for loops!)
+            # KLUDGE: When we had a too short string or otherwise failed return an empty list
             
-            return [" "]
+            return []
 
     wordsplit = text.split()
     result = []
