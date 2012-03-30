514b2372534c75b2598ab7a0165eaeaf11099a82
Anton Kindestam <bountyjedi@gmail.com>
2012-03-30 13:46:56 +0200

    scrape now handles a twittererror when it happens when the.get_public_tweeters()

--- src/scrape/scrape.py
+++ src/scrape/scrape.py
@@ -80,7 +80,13 @@ def gather_data_loop(request_per_hour = 3600, users_to_add = 21):
     
     while(added_users < users_to_add):
         # The set of users which will be added.
-        set_to_add = th.get_public_tweeters()
+        try: 
+            set_to_add = th.get_public_tweeters()
+        except twitter.TwitterError as err:
+            sys.stderr("Got TwitterError while trying to get public timeline " + str(err) + "\n")
+            traceback.print_exc()
+            time.sleep(100)
+            continue            # retry the loop
         
         print "These will be added:"
         for s in set_to_add:
