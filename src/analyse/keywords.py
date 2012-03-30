4356d9aceafdf75bc93c84ba0901914773972364
RaxXo <obodemyr@kth.se>
2012-03-30 11:30:36 +0200

    Added comments

--- src/analyse/keywords.py
+++ src/analyse/keywords.py
@@ -13,7 +13,8 @@ from stopwords import filter_keywords, strip_tweet
 from common import *
 
 def extract_keywords_grammar(text):
-    '''Uses chunks matching to identify keywords in a tweet. The code looks much nicer this way :P'''
+    '''Uses chunks matching to identify keywords in a tweet. The code looks much nicer this way :P
+    Matches grammar in a sentence for potential keywords'''
     
     sequence = nltk.pos_tag(nltk.word_tokenize(text))
     words = []
