35d16b033f01d8787fef12929b8d8be05921724a
Anton Kindestam <bountyjedi@gmail.com>
2012-03-30 21:34:32 +0200

    Forgot to import string

--- src/analyse/keywords.py
+++ src/analyse/keywords.py
@@ -12,6 +12,7 @@ from nltk.tag import _POS_TAGGER
 from stopwords import filter_keywords, strip_tweet
 from common import *
 import operator
+import string
 
 def extract_keywords_grammar(text):
     '''Uses chunks matching to identify keywords in a tweet. The code looks much nicer this way :P'''
