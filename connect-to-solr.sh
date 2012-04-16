4cd0559cb89e06c5477095426ac60a8137ea785c
Anton Kindestam <bountyjedi@gmail.com>
2012-04-16 14:33:56 +0200

    slightly helpful script output

--- connect-to-solr.sh
+++ connect-to-solr.sh
@@ -6,4 +6,7 @@
 # request access (currently granted to MVK group only, if you are not
 # please set up your own Solr)
 
+
+echo "--- Connect to Solr at http://localhost:8888/solr/ ---"
+
 ssh -N -p 22022 -L 8888:localhost:8888 sshguest@xantoz.failar.nu
