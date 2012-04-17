b54a3c4b2c2c12662a77f8c452f9587bb9a43693
Anton Kindestam <bountyjedi@gmail.com>
2012-04-17 22:32:43 +0200

    Newlines after the rate limit exceeded messages

--- src/update/update.py
+++ src/update/update.py
@@ -79,7 +79,7 @@ def main():
                     
                     #If the rate limit was exceeded, pause for 1h1min and try again.
                     except addalyse.AddalyseRateLimitExceededError as err:
-                        sys.stderr.write("RateLimitExceeded, trying again in " + str(CONFIG.get_rate_limit_exceeded_time()) + " seconds.")
+                        sys.stderr.write("RateLimitExceeded, trying again in " + str(CONFIG.get_rate_limit_exceeded_time()) + " seconds.\n")
                         time.sleep(CONFIG.get_rate_limit_exceeded_time())
                         retry = True
                     
