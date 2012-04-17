b01518c9f10265b2bba8703cd190c3dcdbec2f3b
RaxXo <obodemyr@kth.se>
2012-04-17 15:10:28 +0200

    Updated manualtag to tag for eternity

--- src/analyse/manualtag.py
+++ src/analyse/manualtag.py
@@ -3,13 +3,14 @@ from xml.sax.saxutils import unescape
 writefile = open('manual', 'a')
 api = twitter.Api()
 option = raw_input("What would you like to search for? To stop, press enter\n")
+page = 1;
 while option !="":
-    searchresults = api.GetSearch(option)
- 
+    searchresults = api.GetSearch(term=option, per_page=100, lang="en",page=page)
+    page = page+1
     for line in searchresults:
         line = unescape(line.text)
         print line
-        var = raw_input("Enter an option: [Positive: '+', Negative: '-', Neutral: '0', Skip: 's'] \n")
+        var = raw_input("Enter an option: [Positive: '+', Negative: '-', Neutral: '0', Skip: 's', Quit: 'q'] \n")
         if var == '+':
             a = (line, "positive")
             writefile.write(repr(a))
@@ -22,6 +23,8 @@ while option !="":
             a = (line, "neutral")
             writefile.write(repr(a))
             writefile.write("\n")
+        if var == 'Q':
+            break
             
     print "DONE!"
     
