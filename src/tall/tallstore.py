a00dc71dee2ff40f7b074dd73af06b27fa8dc153
Anton Kindestam <bountyjedi@gmail.com>
2012-04-18 22:18:19 +0200

    Patched up so the logic works better (since it is, in both cases, the lack of userlovekeywords on which tallstore will flinch.) what we have now is an ugly kludge but lack of lovekeywords is rare however. The code for generateing the queries should be rewritten in some manner for better support of userlovekeywords =0= [] and/or userhatekeywords == []

--- src/tall/tallstore.py
+++ src/tall/tallstore.py
@@ -126,6 +126,7 @@ def get_frienemies_by_id(username, test_mode=False):
     userlovekeywords = sorted(searchee.lovekeywords_list, key=lambda (a,b): b, reverse=True)[0:500]
     userhatekeywords = sorted(searchee.hatekeywords_list, key=lambda (a,b): b, reverse=True)[0:500]
 
+    # TODO: handle no userlovekeywords case better (at least when we have userhatekeywords)
     if userlovekeywords:
         query = SOLR_INTERFACE.Q(lovekeywords=userlovekeywords[0][0]) ** userlovekeywords[0][1]
         for keyword, weight in userlovekeywords[1:]:
@@ -141,9 +142,11 @@ def get_frienemies_by_id(username, test_mode=False):
         except Exception as e:
             return other_fail(e)
     else:
+        sys.stderr.write("Warning: User has no lovekeywords so we did not output any friends\n")
         friends = []
 
-    if userhatekeywords:
+    # TODO: handle no userlovekeywords case better (at least when we have userhatekeywords)
+    if userlovekeywords:
         query = SOLR_INTERFACE.Q(hatekeywords=userlovekeywords[0][0]) ** userlovekeywords[0][1]
         for keyword, weight in userlovekeywords[1:]:
             query = query | SOLR_INTERFACE.Q(hatekeywords=keyword) ** weight
@@ -158,6 +161,7 @@ def get_frienemies_by_id(username, test_mode=False):
         except Exception as e:
             return other_fail(e)
     else:
+        sys.stderr.write("Warning: User has no lovekeywords so we did not output any enemies either\n")
         enemies = []
 
     
